5c9c688516d4da8588fa7eae3e38ed89c01f3017
lusbnz <vietdq.b21cn788@stu.ptit.edu.vn>
2025-01-13 00:22:22 +0700

    update

--- routes/MainTabs.js
+++ routes/MainTabs.js
@@ -1,4 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialIcons } from "@expo/vector-icons";
 import NewsScreen from "../screens/main/NewsScreen";
 import ProfileScreen from "../screens/main/ProfileScreen";
 import DashboardScreen from "../screens/main/DashboardScreen";
@@ -9,32 +10,49 @@ const Tab = createBottomTabNavigator();
 
 export default function MainTabs() {
   return (
-    <Tab.Navigator initialRouteName="News">
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="News"
-        component={NewsScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Promo"
-        component={PromoScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Scan"
-        component={ScanScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Dashboard"
-        component={DashboardScreen}
-      />
-      <Tab.Screen
-        options={{ headerShown: false }}
-        name="Profile"
-        component={ProfileScreen}
-      />
+    <Tab.Navigator
+      initialRouteName="News"
+      screenOptions={({ route }) => ({
+        tabBarStyle: { backgroundColor: "#248A3D" },
+        tabBarActiveTintColor: "#FFFFFF",
+        tabBarInactiveTintColor: "#C0C0C0",
+        headerShown: false,
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case "News":
+              iconName = "home";
+              break;
+            case "Promo":
+              iconName = "local-offer";
+              break;
+            case "Scan":
+              iconName = "qr-code-scanner";
+              break;
+            case "Dashboard":
+              iconName = "dashboard";
+              break;
+            case "Profile":
+              iconName = "person";
+              break;
+            default:
+              iconName = "circle";
+          }
+
+          return <MaterialIcons name={iconName} size={size} color={color} />;
+        },
+        gestureEnabled: true,
+      })}
+      tabBarOptions={{
+        swipeEnabled: true,
+      }}
+    >
+      <Tab.Screen name="News" component={NewsScreen} />
+      <Tab.Screen name="Promo" component={PromoScreen} />
+      <Tab.Screen name="Scan" component={ScanScreen} />
+      <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
 }
--- screens/main/NewsScreen.js
+++ screens/main/NewsScreen.js
@@ -1,37 +1,34 @@
-import { StatusBar } from "expo-status-bar";
-import { Animated, Text, TouchableOpacity, View } from "react-native";
-import { useAuth } from "../../contexts/AuthContext";
-import { useEffect, useRef } from "react";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 export default function NewsScreen() {
-  const { setIsLoggedIn } = useAuth();
-  const slideAnim = useRef(new Animated.Value(200)).current;
-
-  useEffect(() => {
-    Animated.timing(slideAnim, {
-      toValue: 0,
-      duration: 200,
-      useNativeDriver: true,
-    }).start();
-  }, [slideAnim]);
-
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Animated.View style={[{ transform: [{ translateY: slideAnim }] }]}>
-        <Text style={{textAlign: 'center'}}>NewsScreen</Text>
-        <TouchableOpacity
-          style={{
-            backgroundColor: "#248A3D",
-            padding: 10,
-            borderRadius: 5,
-            marginTop: 10,
-          }}
-          onPress={() => setIsLoggedIn(false)}
-        >
-          <Text style={{ color: "white" }}>Go to Login</Text>
-        </TouchableOpacity>
-        <StatusBar style="auto" />
-      </Animated.View>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 21.0219,
+          longitude: 105.8291,
+          latitudeDelta: 0.01, 
+          longitudeDelta: 0.01, 
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 21.0219, longitude: 105.8291 }}
+          title="Quận Đống Đa"
+          description="Đây là vị trí của Đống Đa, Hà Nội."
+        />
+      </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    flex: 1,
+  },
+});
